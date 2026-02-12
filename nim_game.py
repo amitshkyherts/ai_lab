46e30ae0b7e988e56dc260072faa64a22dacbeb5
amitshky <amitshkymail@gmail.com>
2026-02-12 13:02:23 +0000

    nim best

--- nim_game.py
+++ nim_game.py
@@ -1,7 +1,4 @@
-total_sticks = 13
-
-
-def nim(current_sticks):
+def nim_best(current_sticks):
     num = current_sticks % 4
     if num == 0:
         num = 3
@@ -9,20 +6,28 @@ def nim(current_sticks):
     return num
 
 
-while (total_sticks):
-    print(f"remaining sticks: {total_sticks}")
-    num_sticks = int(input("pick either 1, 2, or 3 sticks: "))
-    if num_sticks < 0 or num_sticks > 3:
-        print("invalid num of sticks picked, try again")
-        continue
+def main():
+    total_sticks = 5
 
-    total_sticks -= num_sticks
-    if total_sticks == 0:
-        print("you win!")
+    while (total_sticks > 0):
+        print(f"remaining sticks: {total_sticks}")
+        num_sticks = int(input("pick either 1, 2, or 3 sticks: "))
+        if num_sticks < 0 or num_sticks > 3 or num_sticks > total_sticks:
+            print("invalid number of sticks picked, try again")
+            continue
 
-    computer = nim(total_sticks)
-    print(f"the computer picked: {computer}")
-    total_sticks -= computer
-    if total_sticks == 0:
-        print("computer wins!")
+        total_sticks -= num_sticks
+        if total_sticks == 0:
+            print("you win!")
+            break
 
+        computer = nim_best(total_sticks)
+        print(f"the computer picked: {computer}")
+        total_sticks -= computer
+        if total_sticks == 0:
+            print("computer wins!")
+            break
+
+
+if __name__ == "__main__":
+    main()
